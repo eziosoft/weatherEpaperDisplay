82b066656612455cdb428a23cf259521f0d79a59
ezio <eziotest@gmail.com>
2020-06-28 14:37:39 +0200

    first commit

---
+++ e_paper_logo.ino
@@ -0,0 +1,167 @@
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <StreamString.h>
+#include "ESP8266WiFi.h"
+#define PrintString StreamString
+
+#define ENABLE_GxEPD2_GFX 0
+#include <GxEPD2_3C.h>
+#include "e.c"
+#include "r.c"
+
+ADC_MODE(ADC_VCC);
+
+struct bitmap_pair
+{
+  const unsigned char *black;
+  const unsigned char *red;
+};
+
+//GxEPD2_3C<GxEPD2_260c, GxEPD2_260c::HEIGHT> display(GxEPD2_260c(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
+GxEPD2_BW<GxEPD2_260, GxEPD2_260::HEIGHT> display(GxEPD2_260(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
+PrintString vddString;
+PrintString rssiString;
+float vdd, rssi;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+  delay(100);
+  display.init(115200);
+  display.setFullWindow();
+  // drawBitmap();
+  // display.powerOff();
+
+  setupWifi();
+
+  vdd = ESP.getVcc() / 1000.0;
+  vddString.print(vdd, 2);
+  Serial.println(vddString);
+
+  rssi = dBmtoPercentage(WiFi.RSSI());
+  rssiString.print(rssi, 0);
+  Serial.println(rssi);
+}
+
+void loop()
+{
+  loopWifi();
+}
+
+void update(char *text)
+{
+   printText1(text);
+  display.hibernate();
+  sendTelemetry();
+
+  delay(100);
+  ESP.deepSleep(60e6);
+}
+
+void printText1(char *text)
+{
+  //Serial.println("helloWorld");
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
+  display.getTextBounds(text, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center bounding box by transposition of origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+
+    printStatic();
+
+    display.setCursor(0, y);
+    display.print(text);
+  } while (display.nextPage());
+  //Serial.println("helloWorld done");
+}
+
+void printStatic()
+{
+  display.setCursor(250, 11);
+  display.print(vddString);
+
+  display.setCursor(215, 11);
+  display.print(rssiString);
+}
+
+void printText(char *text)
+{
+  //Serial.println("helloWorld");
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
+  display.getTextBounds(text, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center bounding box by transposition of origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+
+    printStatic();
+
+    display.setCursor(x, y);
+    display.print(text);
+  } while (display.nextPage());
+  //Serial.println("helloWorld done");
+}
+
+void drawBitmap()
+{
+
+  bitmap_pair bitmap_pairs[] =
+      {
+          {gImage_e, gImage_r}};
+
+  for (uint16_t i = 0; i < sizeof(bitmap_pairs) / sizeof(bitmap_pair); i++)
+  {
+    display.firstPage();
+    do
+    {
+      display.fillScreen(GxEPD_WHITE);
+      display.drawInvertedBitmap(0, 0, bitmap_pairs[i].black, display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
+      //      display.drawBitmap(0, 0, bitmap_pairs[i].red, display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_RED);
+    } while (display.nextPage());
+    delay(2000);
+  }
+}
+
+
+
+const int RSSI_MAX = -50;  // define maximum strength of signal in dBm
+const int RSSI_MIN = -100; // define minimum strength of signal in dBm
+int dBmtoPercentage(int dBm)
+{
+  int quality;
+  if (dBm <= RSSI_MIN)
+  {
+    quality = 0;
+  }
+  else if (dBm >= RSSI_MAX)
+  {
+    quality = 100;
+  }
+  else
+  {
+    quality = 2 * (dBm + 100);
+  }
+
+  return quality;
+} //dBmtoPercentage
---
+++ wifi.ino
@@ -0,0 +1,110 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+
+const char *ssid = "Epaper";              //robot creates wifi hotspot when wifi connection is not configured
+const char *outTopic = "epaper/out";      //MQTT topic for robot telemetry messages
+const char *inTopic = "epaper/in";        //MQTT topic for control messages
+const char *mqtt_server = "192.168.0.19"; //my  MQTT server
+const char *mqtt_server1 = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient); //MQTT
+char buffer1[20];               //multiusage
+
+void configModeCallback(WiFiManager *myWiFiManager);
+
+void setupWifi()
+{
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModeCallback);
+
+  // printText("Connecting...");
+  if (!wifiManager.autoConnect(ssid))
+  {
+    Serial.println("failed to connect and hit timeout");
+    printText("failed to connect and hit timeout");
+    delay(100);
+    ESP.deepSleep(10e6);
+  }
+
+  // printText(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(mqttCallback);
+}
+
+void loopWifi()
+{
+  //MQTT
+  if (!client.connected())
+    reconnect();
+  else
+    client.loop();
+
+  //send telemetry every 200ms
+  // if (millis() % 10000 == 0)
+  // {
+  //   sprintf(buffer1, "T;%s;%d;RSSI=%d;Bat=%s", ssid, millis() / 1000, WiFi.RSSI(),vddString);
+  //   client.publish(outTopic, buffer1);
+  // }
+}
+
+void sendTelemetry()
+{
+  char buf[50];
+  sprintf(buf, "T;%s;%d;RSSI=%d;Bat=%d", ssid, millis() ,(int)rssi, (int)(vdd*100));
+  client.publish(outTopic, buf);
+}
+
+void configModeCallback(WiFiManager *myWiFiManager)
+{
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  printText("Configuration");
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  char buf[length];
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+    buf[i] = (char)payload[i];
+  }
+  Serial.println();
+
+  update(buf);
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ssid))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish(outTopic, "Tank READY");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
